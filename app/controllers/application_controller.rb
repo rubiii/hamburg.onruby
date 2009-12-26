53d6e8197cbc0764bc3443f524fd8e884121e2d2
rubiii <me@rubiii.com>
2009-12-26 18:11:48 +0100

    default to subdomain hamburg.onruby (make sure to setup a virtual host for development)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,25 @@
 class ApplicationController < ActionController::Base
 
-  helper :all
   protect_from_forgery
-
   filter_parameter_logging :password
 
+  helper :all
   helper_method :current_user, :current_user?
 
+  before_filter :subdomain, :validate_subdomain
+
 private
+
+  # Sets the subdomain.
+  def subdomain
+    @subdomain = request.subdomains.first
+  end
+
+  # Redirects requests without subdomain to hamburg.onruby.
+  def validate_subdomain
+    redirect_to "http://hamburg.#{request.domain}#{request.port_string}" and
+      return unless @subdomain == "hamburg"
+  end
 
   # Returns the logged in user session.
   def current_user_session
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ActiveRecord::Base
   has_friendly_id :username
   has_gravatar
 
+  validates_presence_of :username
+  validates_format_of :username, :with => /^[A-Za-z0-9-]+$/,
+    :message => 'The subdomain can only contain alphanumeric characters and dashes.',
+    :allow_blank => true
+  validates_uniqueness_of :username, :case_sensitive => false
+
 end
