a3af8d60647b6862df125b6cde8f7699ea9a0355
Thilko Richter <thilkorichter@Macintosh.local>
2009-12-29 17:17:53 +0100

    add rspec for user and fixtures

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,15 @@
 class User < ActiveRecord::Base
 
-  acts_as_authentic
+  acts_as_authentic do |c|
+    c.require_password_confirmation = false
+  end
+
   has_friendly_id :username
   has_gravatar :email, :size => 60
 
   has_many :projects
   accepts_nested_attributes_for :projects, :allow_destroy => true, :reject_if => :all_blank
 
-  validates_presence_of :username
-  validates_uniqueness_of :username, :case_sensitive => false
-
-  validates_presence_of :email
   validates_presence_of :password
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,30 +1,79 @@
 require File.join(File.dirname(__FILE__), "..", "spec_helper")
 
 describe User do
-  before { @user = User.new }
+  fixtures :users, :projects
 
-  describe "validates" do
-    it "that the username contains at least 3 characters" do
-      @user.username = "ab"
-
-      @user.should have(1).errors_on(:username)
-      @user.should_not be_valid
-    end
-
-    it "that the username does not contain invalid characters" do
-      @user.username = "dude?"
-
-      @user.should have(1).errors_on(:username)
-      @user.should_not be_valid
-    end
-
-    it "that the username is unique" do
-      User.create! :username => "dude", :email => "dude@example.com", :password => "secret"
-      @user = User.new :username => "dude", :email => "dude@ruby.com", :password => "topsecret"
-
-      @user.should have(1).errors_on(:username)
-      @user.should_not be_valid
-    end
+  before do 
+    @user = User.new 
+    @project = projects(:iphone)
   end
 
+  it "validates that the username contains at least 3 characters" do
+    @user.username = "ab"
+
+    @user.should have(1).errors_on(:username)
+  end
+
+  it "validates that the username does not contain invalid characters" do
+    @user.username = "dude?"
+
+    @user.should have(1).errors_on(:username)
+  end
+
+  it "validates that the username is unique" do
+    @user = User.new :username => users(:dette).username, :email => "wtf@example.com", :password => "wtfwtfwtf"
+
+    @user.should have(1).errors_on(:username)
+  end
+
+  it "finds a user by username as the id" do
+    @user = User.find "dette"
+    @user.username.should eql "dette"
+  end
+
+  it "has a gravatar url" do
+    @user.gravatar_url.should_not be_nil
+  end
+
+  it "returns the username as the param" do
+    @user.username = "dude"
+    @user.to_param.should eql "dude"
+  end
+
+  it "should have many projects" do
+    User.reflect_on_association(:projects).should_not be_nil
+  end
+
+  it "should cascade projects" do
+    @user = users(:dette)
+    @user.projects << @project
+    @user.save
+    User.find("dette").projects.first.id.should eql @project.id
+  end
+
+  it "should allow projects to be destroyed" do
+    @user = users(:dette)
+    @user.projects = []
+    @user.save
+    Project.exists?(1).should be_false
+  end
+
+  it "should validate the password" do
+    @user = users(:dette)
+    @user.password = ""
+    @user.should have(1).errors_on(:password)
+  end
+
+  it "should validate the email has at least 3 chars" do
+    @user = users(:dette)
+    @user.email = "ab3"
+    @user.should have(2).errors_on(:email)
+  end
+  
+  it "should validate the email has a valid format" do
+    @user = users(:dette)
+    @user.email = "ab3sdlajfkldskjflks"
+    @user.should have(1).errors_on(:email)
+  end
+  
 end
